4a370f6849542ec7299a80fea5a5be945714dec9
Kaze Wong <kazewong.physics@gmail.com>
2024-08-26 22:36:49 -0400

    scaffold resource

--- LA2028/__init__.py
+++ LA2028/__init__.py
@@ -1,10 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
 from .assets import assets
-from .jobs import do_stuff
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
-    jobs=[do_stuff],
 )

c510f78f9e39527fce2b6b0e59d1070073bc9e7c
Kaze Wong <kazewong.physics@gmail.com>
2024-08-28 20:45:57 -0400

    update postgres_io and add graph video asset

--- LA2028/assets/core/raw_video.py
+++ LA2028/assets/core/raw_video.py
@@ -1,9 +1,24 @@
-from dagster import asset
+from dagster import asset, graph_asset, op, Config
 from ...resources.minio_io import MinioResource
+from ...resources.postgres_io import PostgresResource
 
-@asset
-def ListMinioObjects(minio: MinioResource) -> list:
-    return minio.list_objects("highjump")
+class GetVideoByDateConfig(Config):
+    start_date: str
+    end_date: str
+
+@op
+def GetVideoByDate(postgres: PostgresResource, config: GetVideoByDateConfig) -> list:
+    return postgres.selectVideoByDate(config.start_date, config.end_date)
+
+@graph_asset
+def ListVideos():
+    return GetVideoByDate()
+
+# @graph_asset
+# def ListMinioObjects(minio: MinioResource):# -> list:
+#     print(1)
+#     return []
+    # return minio.list_objects("highjump")
 
 # @asset
 # def GetMinioObject(bucket: str, object_name: str) -> str:
--- LA2028/resources/postgres_io.py
+++ LA2028/resources/postgres_io.py
@@ -11,6 +11,13 @@ class PostgresResource(ConfigurableResource):
     password: str
 
     def selectVideoByDate(self, start_date: str, end_date: str) -> TupleRow:
-        cursor = self.client.cursor()
-        cursor.execute("SELECT * FROM videos WHERE cast(to_timestamp(start_time/1000) as date) BETWEEN %s AND %s ORDER BY start_time DES", (start_date, end_date))
+        client = psycopg.connect(
+            host=self.host,
+            port=self.port,
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password
+        )
+        cursor = client.cursor()
+        cursor.execute("SELECT * FROM videos WHERE cast(to_timestamp(start_time/1000) as date) BETWEEN %s AND %s ORDER BY start_time DESC", (start_date, end_date))
         return cursor.fetchall()

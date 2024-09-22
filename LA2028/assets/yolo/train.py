c35e39020295b55b6f79aab77ec3b708c086cf9b
Kaze Wong <kazewong.physics@gmail.com>
2024-09-21 22:36:47 -0400

    update training config

--- LA2028/assets/yolo/train.py
+++ LA2028/assets/yolo/train.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 
 @asset(deps=["training_data", "yaml"])
 def trained_yolo_model():
-    model = YOLO("./data/models/yolov8n-cls.pt")
-    result = model.train(data='data/annotated_frames',batch=16, epochs=100)
+    model = YOLO("./data/models/yolov8l-cls.pt")
+    result = model.train(data='data/annotated_frames',batch=16, epochs=100, project='data', name='finetuned_yolo_classifier')
     return result
 
 train_yolo_model_job = define_asset_job(name="train_yolo_model_job", selection="trained_yolo_model")

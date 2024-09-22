40d8f9a5aa2e32c0b0790bc5a7bb9792e5666811
Kaze Wong <kazewong.physics@gmail.com>
2024-09-21 22:27:09 -0400

    update training path

--- LA2028/assets/yolo/train.py
+++ LA2028/assets/yolo/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 @asset(deps=["training_data", "yaml"])
 def trained_yolo_model():
-    model = YOLO("yolov8n-cls.pt")
+    model = YOLO("./data/models/yolov8n-cls.pt")
     result = model.train(data='data/annotated_frames',batch=16, epochs=100)
     return result
 

1241a62b26e41d3da8a396675238a53098c75795
Kaze Wong <kazewong.physics@gmail.com>
2024-12-11 22:21:03 -0500

    fix: update output path for finetuned YOLO classifier model

--- LA2028/assets/contact_detection/contact_detection_model.py
+++ LA2028/assets/contact_detection/contact_detection_model.py
@@ -8,7 +8,7 @@ class YOLOConfig(Config):
 @asset(deps=["training_data", "dataset_description_yaml"])
 def trained_yolo_model(config: YOLOConfig):
     model = YOLO(config.output_path + "models/yolov8l.pt")
-    result = model.train(data=config.data_path + 'data.yaml',batch=16, epochs=100, project='data', name='finetuned_yolo_classifier')
+    result = model.train(data=config.data_path + 'data.yaml',batch=16, epochs=100, project='data', name=config.output_path+'finetuned_yolo_classifier')
     return result
 
 train_yolo_model_job = define_asset_job(name="train_yolo_model_job", selection="trained_yolo_model")
